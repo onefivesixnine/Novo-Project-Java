de8bd5634efc67a51513cd118dd42100d10d042b
Thiago s Costa <144514666+onefivesixnine@users.noreply.github.com>
2024-05-21 02:18:26 -0300

    contaterminal.java

--- src/contaterminal.java
+++ src/contaterminal.java
@@ -1,5 +1,26 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+
+public class contaterminal{
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        // Obtendo dados do usuário
+        System.out.print("Informe o número da conta : ");
+        int numeroConta = entrada.nextInt();
+
+        System.out.print("Informe o número da agência : ");
+        char agencia = entrada.next().charAt(0);
+
+        System.out.print("Informe o seu nome : ");
+        String nomeCliente = entrada.nextLine();
+
+        System.out.print("Informe o saldo inicial: ");
+        float saldo = entrada.nextFloat();
+
+
+        // Mensagem final
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco!");
+        System.out.println("Sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
